71895b98a7ffd2a5a4daf72eb47bc96b04cf75f1
RullDev <105213064+RullDev@users.noreply.github.com>
2022-10-30 09:59:18 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -15,7 +15,7 @@ let str = `${global.wm}
 │ • *Ovo:* [${global.povo}]
 │ • *Link Aja:* [${global.plinkaja}]
 ❏────`
-let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
+let wibu = `https://telegra.ph/file/762d68057066bc71ed540.png` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/khoirul36_",
